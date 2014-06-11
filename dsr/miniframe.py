4d84d6209e1a845c6302ee4262672878776b4f2f
Pawel Murias <pawelmurias@gmail.com>
2014-06-11 19:19:13 +0200

    Add .gitignore file.

--- dsr/miniframe.py
+++ dsr/miniframe.py
@@ -1,16 +1,50 @@
 class PayloadTooLargeError(Exception):
     pass
 
+def computeCRC_8(data, pattern = '0xD5'):
+    div = 256 + int(pattern, 16)
+    x = 0
+    for c in data:
+        x = (x<<8) + ord(c)
+        for i in [7,6,5,4,3,2,1,0]:
+            if x >> (i+8):
+                x ^= div << i
+        assert x < div
+    return x
+
 class MiniFrame:
-#    @classmethod
-    def unpack(payload):
-        frame = Frame()
+    @classmethod
+    def unpack(self,packed):
+        length = ord(packed[0])
+        content = packed[2:2+length]
+
+
+        frame = MiniFrame(packed[1],content)
+        frame.valid = packed[2+length] == chr(computeCRC_8(packed[0:2+length]))
+
+
         return frame
 
-    def __init__(self,payload):
+    def __init__(self,frameType,payload):
         self._payload = payload
+        self._frameType = frameType
 
     def pack(self):
         l = len(self._payload)
+
         if l > 255:
             raise PayloadTooLargeError('Payload of length %d is too long, maximum is 255' % l)
+
+        bytes = chr(l) + self._frameType + self._payload
+        bytes += chr(computeCRC_8(bytes))
+
+
+        return bytes
+
+    @property
+    def payload(self):
+        return self._payload
+
+    @property
+    def frameType(self):
+        return self._frameType
--- t/miniframe.py
+++ t/miniframe.py
@@ -4,8 +4,32 @@ from dsr.miniframe import *
 class TestMiniFrame(unittest.TestCase):
     def test_too_long_payload(self):
         with self.assertRaises(PayloadTooLargeError):
-            frame = MiniFrame('abcd' * 100)           
+            frame = MiniFrame('?','abcd' * 100)           
             packed = frame.pack()
+
+    def test_accessors(self):
+        frame = MiniFrame('x','0123abc')
+        self.assertEqual(frame.frameType,'x')
+
+    def test_packing_and_unpacking(self):
+        frame = MiniFrame('x','0123abc')
+        packed = frame.pack()
+
+        unpacked = MiniFrame.unpack(packed)
+        self.assertEqual(unpacked.frameType,'x')
+        self.assertEqual(unpacked.payload,'0123abc')
+
+    def test_checksum(self):
+        frame = MiniFrame('x','0123abc')
+        packed = frame.pack()
+
+        unpacked = MiniFrame.unpack(packed)
+        self.assertTrue(unpacked.valid)
+
+        wrong = packed[:3] + packed[2] + packed[4:]
+
+        self.assertFalse(MiniFrame.unpack(wrong).valid)
+
 
 
 if __name__ == '__main__':
